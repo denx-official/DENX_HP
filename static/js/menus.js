623c3eaecbaa4320431f5f1f503d81f2f8b6849a
ippee <ippee.music@gmail.com>
2020-11-13 18:28:48 +0900

    メニューバーの不具合を修正

---
+++ static/js/menus.js
@@ -0,0 +1,13 @@
+if (window.matchMedia('(max-width: 768px)').matches) {
+    var menusItems = ['about', 'departments', 'products', 'caret1', 'caret2', 'caret3'];
+
+    for (var i = 0, len = menusItems.length; i < len; ++i) {
+        var dom_obj = document.getElementById(menusItems[i]);
+        var dom_obj_parent = dom_obj.parentNode;
+        dom_obj_parent.removeChild(dom_obj);
+    }
+} 
+
+// else if (window.matchMedia('(min-width:750px)').matches) {
+    //PC処理
+// }
